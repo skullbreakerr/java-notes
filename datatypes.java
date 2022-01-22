dea648ae96b7ba6dc24b929ea874e539de7d31c8
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2022-01-22 16:10:04 +0530

    datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,55 @@
+//;;;;;;;;;;;;;; D A T A T Y P E S  I N  J A V A ;;;;;;;;;;;;;
+
+//===== S T R I N G S =====
+// public class datatypes {
+//     public static void main(String[] args) {
+//         String name = "dharmik";
+//         String name2 = "Patel";
+//         String name3 = name + " " + name2;
+
+//         System.out.println(name3);
+//     }
+// }
+
+// +++++ I N T +++++
+// public class datatypes {
+// public static void main(String[] args) {
+// int rollno = 39, id = 256;//instead of int we can use byte,long,double.//
+
+// System.out.println("Id of student is " + id + " & rollno is " + rollno);
+
+// }
+
+// }
+
+// ----- L O N G -----
+// public class datatypes {
+// public static void main(String[] args) {
+// long phone = 9924198501L;// Put 'L' after storing data for understand of java
+// Compiler//
+
+// System.out.println("phone number of student is " + phone);
+// }
+// }
+
+// >>>>> F L O A T <<<<<
+
+// public class datatypes {
+// public static void main(String[] args) {
+// float pie = 3.14F; // Put 'F' after storing data for understand of java
+// Compiler//
+
+// System.out.println("The value of pie is " + pie);
+// }
+// }
+
+//::::: B O O L E A N :::::
+
+public class datatypes {
+    public static void main(String[] args) {
+        boolean age = true;// it can be true or false only 
+
+        System.out.println(age);
+
+    }
+}
